23a4cc3dfbcb15a5c8bb8c81ec7761368f4606eb
Minoru Osuka <minoru.osuka@gmail.com>
2016-09-07 23:54:31 +0900

    Update.

---
+++ src/test/java/com/github/mosuka/zookeeper/cli/command/LsCommandTest.java
@@ -0,0 +1,34 @@
+package com.github.mosuka.zookeeper.cli.command;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.github.mosuka.zookeeper.cli.util.ZooKeeperTestBase;
+
+public class LsCommandTest extends ZooKeeperTestBase {
+
+  @Test
+  public void testExecute() {
+    Map<String, Object> parameters = new LinkedHashMap<String, Object>();
+    parameters.put("zookeeper_server", zkServerStr);
+    parameters.put("session_timeout", sessionTimeout);
+    parameters.put("pretty_print", false);
+    parameters.put("path", "/");
+    parameters.put("watch", false);
+    parameters.put("with_stat", false);
+
+    int statusCode = 0;
+
+    LsCommand command = new LsCommand();
+    try {
+      statusCode = command.execute(parameters);
+    } catch (Exception e) {
+      fail(e.getMessage());
+    }
+
+    assertEquals(0, statusCode);
+  }
+
+}
