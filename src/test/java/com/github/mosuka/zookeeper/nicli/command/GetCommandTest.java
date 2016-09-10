25d867751fa1311d2f0a09b99bc5f6abcf704ceb
Minoru Osuka <minoru.osuka@gmail.com>
2016-09-10 20:34:52 +0900

    Update.

---
+++ src/test/java/com/github/mosuka/zookeeper/nicli/command/GetCommandTest.java
@@ -0,0 +1,47 @@
+package com.github.mosuka.zookeeper.nicli.command;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.github.mosuka.zookeeper.nicli.util.ZooKeeperConnection;
+import com.github.mosuka.zookeeper.nicli.util.ZooKeeperTestBase;
+
+public class GetCommandTest extends ZooKeeperTestBase {
+    public ZooKeeperConnection zkConnection = null;
+
+    @Override
+    public void before() throws Exception {
+        super.before();
+        zkConnection = new ZooKeeperConnection(server, sessionTimeout);
+    }
+
+    @Override
+    public void after() throws Exception {
+        super.after();
+        zkConnection.close();
+    }
+
+    @Test
+    public void testExecute() {
+        Map<String, Object> parameters = new LinkedHashMap<String, Object>();
+        parameters.put("server", server);
+        parameters.put("session_timeout", sessionTimeout);
+        parameters.put("pretty_print", false);
+        parameters.put("path", "/");
+        parameters.put("watch", false);
+        parameters.put("with_stat", false);
+
+        int statusCode = Command.STATUS_SUCCESS;
+
+        GetCommand command = new GetCommand();
+        try {
+            statusCode = command.execute(parameters);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+
+        assertEquals(Command.STATUS_SUCCESS, statusCode);
+    }
+}
